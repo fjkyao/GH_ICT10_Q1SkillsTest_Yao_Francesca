32037b5034f7da32b6ee4cb7ed623af81eabe56c
sca <yao.fjk@obmontessori.edu.ph>
2025-10-04 09:52:42 +0800

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # skills test: ordering form
+from pyscript import display, document
+
+
+def b_function(e):
+    name = document.getElementById('cname').value
+    place = document.getElementById('address').value
+    number = document.getElementById('contact').value
+
+    display(f'', target='output') 
+
+    
+order_summary = '''
+Order for: {cname}
+Address: {address}
+Contact Number: {contact}
+Total: {}
+'''
+
+display(order_summary)
